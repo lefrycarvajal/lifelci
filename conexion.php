7b4b2f526a6d740a332efe52ada393c9ddb3dce7
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-04-14 20:59:01 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","dbdominicang");
+$conex = mysqli_connect("grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","xk6tdci5789g5yz8","fazzyv2paen1f9px","kbckcl44wragbxyj");
 
 ?>
